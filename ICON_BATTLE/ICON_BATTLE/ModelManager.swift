d4a26c006446aaf7f7f73a57afbdcf3f711a57da
櫻木善将 <ysrhsp@outlook.com>
2015-07-01 20:21:51 +0900

    basic file system

---
+++ ICON_BATTLE/ICON_BATTLE/ModelManager.swift
@@ -0,0 +1,16 @@
+//
+//  ModelManager.swift
+//  ICON_BATTLE
+//
+//  Created by 櫻木善将 on 2015/07/01.
+//  Copyright (c) 2015年 YoshimasaSakuragi. All rights reserved.
+//
+
+import Foundation
+class ModelManager{
+    
+    static let sharedInstance:ModelManager = ModelManager();
+    private init(){};
+    
+
+}
